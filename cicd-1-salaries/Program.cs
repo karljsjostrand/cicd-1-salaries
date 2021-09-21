240028b0e2f9120eed4da6d860822f4f45df3aff
Karl Sjöstrand <karljsjo@gmail.com>
2021-09-21 12:05:37 +0200

    Add project files.

---
+++ cicd-1-salaries/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace cicd_1_salaries
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
